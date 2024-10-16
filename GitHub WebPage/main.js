b94c2a14bb5edc58595c96afd1de3305e48ab23d
Wong Khye Yang <kywong@greatech-group.com>
2024-10-16 15:09:47 +0800

    Moved Github pages files into a single folder

---
+++ GitHub WebPage/main.js
@@ -0,0 +1,42 @@
+console.log('Hello from main.js')
+
+// this is a commment
+
+// These are variables - use let, const to declare variables
+let age = 25 // int variable
+const pi = 3.142 // constant variable declaration
+
+// Data Types
+// Strings
+let someString = 'hello world'
+let anotherString = "Javascript!!"
+let backTickString = `another string with back quotes`
+
+// Number Types
+let someNumber = 123
+
+// Bools
+let someBool = true
+let falseBoool = false
+
+// Undefined - variables with values NOT Assigned (or YET to be assigned. Definitely NOT NULL.)
+let thisIsAnUndefined
+console.log(thisIsAnUndefined) // prints undefined
+
+let alsoUndefined = undefined // explicitly declaring undefined
+
+// Null - a variable DOES NOT have a value
+let iAmANull = null
+
+// Dictionary
+let person = {
+    firstName:'steve',
+    age:300,
+}
+
+console.log(person.age)
+
+// Arrray
+let myArr = [1,2,3,4,5,56]
+console.log(myArr[0])
+
